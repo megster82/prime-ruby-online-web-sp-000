0e07a9342fe037ebd02a80a06ed52e4926094969
meaghan jennings <meaghanljennings@gmail.com>
2020-07-28 21:36:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1 || integer == 0 || integer == 1
+    return false
+  elsif
+    (2..integer - 1).each do |i|
+      if integer % i == 0
+        return false
+      end
+    end
+  end
+  true 
+end
