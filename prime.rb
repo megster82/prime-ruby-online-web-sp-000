8504afd248d170500a366dd4ddb05f1d0d7e22dc
meaghan jennings <meaghanljennings@gmail.com>
2021-03-03 21:57:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(integer)
   if (integer % 2 == 0 || integer % 3 == 0 || integer % 4 == 0 || integer % 5) && integer > 0 == 0 
-    true
+    return false 
   else
-    false
+    return true 
   end
 end 
   
